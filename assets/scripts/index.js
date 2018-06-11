82c8acb77dc1d10ef07a8e0b3fda04ee3c311286
samprimary <samprimary@gmail.com>
2018-06-11 08:49:42 -0400

    Added first working on-click event listeners and colors
    - all colors using SASS standard

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,10 +2,48 @@
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
-
+const authEvents = require('./events')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+
+   $('#A1').click(function() {
+     console.log("A1, The Top Left Tile Was Clicked")
+   })
+
+   $('#A2').click(function() {
+     console.log("A2, The Top Middle Tile Was Clicked")
+   })
+
+   $('#A3').click(function() {
+     console.log("A3, The Top Left Tile Was Clicked")
+   })
+
+   $('#B1').click(function() {
+     console.log("B1, The Middle Left Tile Was Clicked")
+   })
+
+   $('#B2').click(function() {
+     console.log("B2, The PIVOTAL CENTRAL Tile Was Clicked")
+   })
+
+   $('#B3').click(function() {
+     console.log("B3, The Middle Left Tile Was Clicked")
+   })
+
+   $('#C1').click(function() {
+     console.log("C1, The Bottom Left Tile Was Clicked")
+   })
+
+   $('#C2').click(function() {
+     console.log("C2, The Bottom Middle Tile Was Clicked")
+   })
+
+   $('#C3').click(function() {
+     console.log("C3, The Bottom Left Tile Was Clicked")
+   })
+
 })
+
+// require stuff
